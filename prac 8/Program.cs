5a616a7a90253168703e2d597790630958c44f06
ekelemene <ksuxaskrip2007@gmail.com>
2024-12-10 00:36:28 +0300

    Add files via upload

---
+++ prac 8/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1. Заполнить одномерный массив из 8 целых чисел с клавиатуры и вывести его на экран.
+            //int[] omas = new int[8];
+            //Console.WriteLine("заполнить одномерный массив");
+            // for(int i = 0;i<omas.Length;i++)
+            // {
+            //    Console.Write($"omas[{i}] = ");
+            //    omas[i] = int.Parse(Console.ReadLine());
+            // }
+            //Console.WriteLine("вывод одномерного массива");
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    Console.WriteLine($"omas[{i}] = {omas[i]}");
+            //}
+
+            //Задача 2.Составьте программу ввода(без клавиатуры) и вывода массива: 5, -2, 3, -6, 7, 9, -4, 8, 1, 2, 7, -3.
+            //int[] omas = { 5, -2, 3, -6, 7, 9, -4, 8, 1, 2, 7, -3 };
+            //Console.WriteLine("вывод массива");
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    Console.WriteLine($"omas[{i}] = {omas[i]}");
+            //}
+
+            //Задача 3.Массив предназначен для хранения значений ростов двенадцати человек.С помощью датчика случайных чисел
+            //заполнить массив целыми значениями, лежащими в диапазоне от 163 до 190 включительно.
+            //int[] omas = new int[12];
+            //Random rnd = new Random();
+            //for (int i = 0; i <omas.Length; i++)
+            //{
+            //    omas[i] = rnd.Next(163, 190);
+            //    Console.WriteLine($"omas[{i}] = {omas[i]}");
+            //}
+           
+        }
+    }
+}
